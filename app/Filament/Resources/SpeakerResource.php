e9298ac7b65b5435ede5aa763b684ba418521d89
gacmen45 <marekgacek45@gmail.com>
2024-06-16 07:09:59 +0200

    create infolist

--- app/Filament/Resources/SpeakerResource.php
+++ app/Filament/Resources/SpeakerResource.php
@@ -2,18 +2,23 @@
 
 namespace App\Filament\Resources;
 
+use App\Enums\TalkStatus;
 use App\Filament\Resources\SpeakerResource\Pages;
 use App\Filament\Resources\SpeakerResource\RelationManagers;
 use App\Models\Speaker;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Group;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
-
+use Filament\Infolists\Infolist;
 
 
 class SpeakerResource extends Resource
@@ -52,11 +57,57 @@ class SpeakerResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
+            ]);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Section::make('Personal Information')
+                    ->columns(3)
+                    ->schema([
+                        ImageEntry::make('avatar')
+                            ->circular(),
+                        Group::make()
+                            ->columnSpan(2)
+                            ->columns(2)
+                            ->schema([
+                                TextEntry::make('name'),
+                                TextEntry::make('email'),
+                                TextEntry::make('twitter_handle')
+                                    ->label('Twitter')
+                                    ->getStateUsing(function ($record) {
+                                        return '@' . $record->twitter_handle;
+                                    })
+                                    ->url(function ($record) {
+                                        return 'https://twitter.com/' . $record->twitter_handle;
+                                    }),
+                                TextEntry::make('has_spoken')
+                                    ->getStateUsing(function ($record) {
+                                        return $record ? 'No' :'Yes';
+                                        // return $record->talks()->where('status', TalkStatus::APPROVED)->count() > 0 ? "Previous Speaker" : 'Has Not Spoken';
+                                    })
+                                    ->badge()
+                            ])
+
+                    ]),
+                Section::make('Other Information')
+                    ->schema([
+                        TextEntry::make('bio')
+                            ->extraAttributes(['class' => 'prose dark:prose-invert'])
+                            ->html(),
+                        TextEntry::make('qualifications')
+                        ->listWithLineBreaks()
+                        ->bulleted()
+                        ,
+                    ])
             ]);
     }
 
@@ -72,7 +123,8 @@ class SpeakerResource extends Resource
         return [
             'index' => Pages\ListSpeakers::route('/'),
             'create' => Pages\CreateSpeaker::route('/create'),
-            'edit' => Pages\EditSpeaker::route('/{record}/edit'),
+            // 'edit' => Pages\EditSpeaker::route('/{record}/edit'),
+            'view' => Pages\ViewSpeaker::route('/{record}')
         ];
     }
 }
